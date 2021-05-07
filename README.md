2c4978a6fcbbfa03bc457708ad275838b2efe9fe
Sam <samgolovchenko@gmail.com>
2021-05-08 01:51:52 +0300

    webpack

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
     <br>    
     <img 
         alt="Preview"
+<<<<<<< HEAD
         src="https://github.com/iFrosta/SnakeGame/blob/master/img/SnakePreview.png"
         width=500px
     />
   <h1>SnakeGame</h1>
   <strong>Web Snake Game on JS with "Ai"</strong>
+=======
+        src="https://github.com/iFrosta/Projects/blob/master/SnakeGame/src/assets/img/SnakeLogo.png"
+        width=500px
+    />
+  <h1>SnakeGame</h1>
+  <strong>JS, Ai, Autopilot</strong>
+>>>>>>> a015a75 (webpack)
 </div>
---
+++ webpack.config.js
@@ -0,0 +1,214 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const MinifyJSONWebpackPlugin = require('minify-json-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: "all"
+    }
+  }
+  
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserWebpackPlugin(),
+      new MinifyJSONWebpackPlugin()
+    ]
+  }
+  return config
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const cssLoaders = extra => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
+      }
+    },
+    'css-loader'
+  ]
+  
+  if (isProd) {
+    loaders.push(
+      {
+        loader: 'postcss-loader',
+        options: {
+          plugins: () => [
+            autoprefixer()
+          ],
+          sourceMap: true
+        }
+      }
+    )
+  }
+  
+  if (extra) {
+    loaders.push(extra)
+  }
+  
+  return loaders
+}
+
+const babelOptions = preset => {
+  const options = {
+    presets: [
+      '@babel/preset-env'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-class-properties'
+    ]
+  }
+  
+  if (preset) {
+    options.presets.push(preset)
+  }
+  
+  return options
+}
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', './index.js']
+  },
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.png'],
+    alias: {
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@elements': path.resolve(__dirname, 'src/components/elements'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@images': path.resolve(__dirname, 'src/assets/img'),
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    historyApiFallback: true,
+    open: isDev,
+    hot: isDev,
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+        removeComments: isProd
+      }
+    }),
+    require('autoprefixer'),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/assets/projects/*'),
+        to: path.resolve(__dirname, 'dist')
+      },
+      {
+        from: path.resolve(__dirname, 'src/assets/favicon/*'),
+        to: path.resolve(__dirname, 'dist')
+      },
+      {
+        from: path.resolve(__dirname, 'src/data/*'),
+        to: path.resolve(__dirname, 'dist')
+      },
+      {
+        from: path.resolve(__dirname, 'src/assets/img/*'),
+        to: path.resolve(__dirname, 'dist')
+      }
+    ]),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders()
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: cssLoaders({
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                prependData: `@import "@styles/_variables.scss"`,
+                outputStyle: 'compressed',
+                fiber: false
+              }
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: 'postcss.config.js'
+              }
+            }
+          }
+        )
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babelOptions()
+        }
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-typescript')
+        }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: 'babel-loader',
+          options: babelOptions('@babel/preset-react')
+        }
+      }
+    ]
+  }
+}
+
